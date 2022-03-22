ccf906e48bd6bb36eb65531a47fcf0136bf9f54d
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2022-03-22 14:28:50 -0600

    Se crea estructura inicial del sitio

---
+++ src/components/layout.js
@@ -0,0 +1,24 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { GlobalStyles } from '../styles/js/index';
+
+
+const Layout = ({ children }) => (
+
+  <>
+    <meta
+      name='viewport'
+      content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <GlobalStyles />
+    <main>{children}</main>
+  </>
+
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { SEO } from "../components";
+
+
+
+const IndexPage = () => {
+  return (
+    <>
+      Hola
+    </>
+  )
+};
+
+
+export default IndexPage
