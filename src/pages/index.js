a70d68adcce2488da90d2b2b04ba234af53b21ad
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2022-03-25 10:45:58 -0600

    Agregamos fondo en pantalla home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react"
 import { Footer, Home, Navbar } from "../components";
-
+import '../styles/css/navbar.css'
 
 const IndexPage = () => {
   return (
     <>
+      <StaticImage
+        className="fondo"
+        src="../images/fondo.jpg"
+        alt="gaia fondo"
+        loading="eager"
+        layout="fullWidth"
+        breakpoints={[750, 1080, 1366, 1920]}
+        formats={['auto', 'webp', 'avif']}
+        quality='80'
+      />
       <Navbar />
       <Home />
       <Footer />
