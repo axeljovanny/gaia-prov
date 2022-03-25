ec5b36906ffa9dc90a2490ab703a3f4946c05be0
Axel Jovanny Quintero Trujillo <axel.jovanny.aj@gmail.com>
2022-03-25 10:45:01 -0600

    Se agregan constantes y fuentes

---
+++ src/utils/const.js
@@ -0,0 +1,29 @@
+import '../styles/css/fonts.css'
+
+
+export const colors = {
+    background: "#000",
+    softYellow: "#FFFABE",
+    softBlue: "#98F3FF",
+    black: "#020202",
+    white: "#FFFFFF",
+    none: "none"
+
+}
+
+export const size = {
+    Wheader: "calc(100px + 0.4vw)",
+    Mheader: "calc(70px + 0.4vw)",
+    Wtittle: "calc(25px + 0.3vw)",
+    Mtittle: "calc(22px + 0.2vw)",
+    Wtext: "calc(18px + 0.2vw)",
+    Mtext: "calc(16px + 0.1vw)",
+    Wmini: "calc(14px + 0.1vw)",
+    Mmini: "calc(12px + 0.1vw)",
+}
+
+export const font = {
+    bold: "MontserratBold",
+    medium: "MontserratMedium",
+    light: "MontserratLight",
+}
