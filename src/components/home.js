bbc0e2377f35ebd708ff5239a7afad77397f745a
Axel Salgado <axel.salgado55@gmail.com>
2022-03-29 14:17:05 -0600

    Se agregaron y realizaron ajustes para los archivos home
    home.js : Se ajustaron medidas y se eliminaron otras propiedades para evitar la redundancia en los distintos tamaños.
    
    home.css : Se cambiaron el nombre de las clases por etiquetas h1 y h3.

--- src/components/home.js
+++ src/components/home.js
@@ -1,36 +1,46 @@
 import React from "react"
+import { Link } from "gatsby"
 import "../styles/css/home.css"
+import { StaticImage } from "gatsby-plugin-image";
+import { StyledTitleContainer, StyledButtonsContainer, StyledText, StyledHome, StyledButton, StyledLogoMobileContainer } from "../styles/js/home.js"
 
-import { StyledContentContainer, StyledTitleContainer, StyledButtonsContainer, StyledText, StyledHome, StyledButton } from "../styles/js/home.js"
 
 const Home = ({ children }) => (
     <>
         <StyledHome>
-            <StyledContentContainer>
-                <StyledTitleContainer>
+            <StyledTitleContainer>
+                <StyledLogoMobileContainer>
+                    <StaticImage
+                        className="container-logo"
+                        imgClassName="logo"
+                        src="../images/logo.png"
+                        alt="gaia logo"
+                        loading="eager"
+                        layout="constrained"
+                        breakpoints={[750, 1080, 1366, 1920]}
+                        formats={['auto', 'webp', 'avif']}
+                        quality='80'
+                    />
+                </StyledLogoMobileContainer>
+                <h1>Gaia Evolution</h1>
+                <h3>Spa & Salon</h3>
 
-                    <p className="title-gaia-evo">Gaia Evolution</p>
-                    <p className="sub-gaia-evo" >Spa & Salon</p>
-
-                    <StyledButtonsContainer>
-
+                <StyledButtonsContainer>
+                    <Link to="/">
                         <StyledButton className="button">
                             Gift Card
                         </StyledButton>
+                    </Link>
+                    <Link to="/">
                         <StyledButton className="button">
                             AvedaShop
                         </StyledButton>
-                    </StyledButtonsContainer>
-                </StyledTitleContainer>
-
-
-            </StyledContentContainer>
+                    </Link>
+                </StyledButtonsContainer>
+            </StyledTitleContainer>
             <StyledText>
                 NATURAL - ORGANIC - VEGAN - CRUELTYFREE
             </StyledText>
-
-
-
         </StyledHome >
     </>
 
--- src/styles/js/home.js
+++ src/styles/js/home.js
@@ -1,119 +1,150 @@
-import styled from "styled-components"
 import { above } from "."
+import styled from "styled-components"
+import { colors, font, size } from "../../utils/const"
 
 
 export const StyledHome = styled.div`
-  
+  display: flex;
+  justify-content: center ;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
   ${above.medium`
       
   `}
   ${above.large`
-
-    display: flex;
-    justify-content: center ;
-    align-items: center;
-    width: 100vw;
-    height: 75vh;
-    background: none ;
+  
   `}
 `
-
-
 
 export const StyledButton = styled.button`
-  
+  width: 140px;
+  height: 43px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  border-radius: 50px;
+  margin: 1rem auto;
+  justify-content: center ;
+  background-color: transparent;
+  border 1px white solid ;
+  font-family: ${font.bold};
+  color: ${colors.white};
+
   ${above.medium`
       
   `}
   ${above.large`
-    width: 110px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    border-radius: 50px;
+ 
     margin: 0 25px 0 25px;
-    justify-content: center ;
-    background-color: none;
-    border 1px white transparent;
-    font-family: 'Montserrat', sans-serif;
 
   `}
 `
-
 
 export const StyledText = styled.div`
-  
+    display: none;
   ${above.medium`
-      
+
   `}
   ${above.large`
-  float: right;
-  right: 0;
-  height: auto;
-  text-align:center;
-  position: absolute; 
-  width: fit-content;
-  background: none;
-  transform: rotate(90deg) translateY(-110px);
-  font-family: 'Montserrat', sans-serif;
-
-  `}
-`
-
-
-export const StyledContentContainer = styled.div`
-  
-  ${above.medium`
-      
-  `}
-  ${above.large`
-    width: 70%;
-    height: 70vh;
-    display: flex;
-    font-size: 3rem;
+    display: block;
+    right: 0;
+    float: right;
+    height: auto;
     background: none;
-    align-items: center;
-    justify-content: center ;
-    font-family: 'Montserrat', sans-serif;
+    text-align:center;
+    width: fit-content;
+    position: absolute; 
+    color: ${colors.white};
+    transform: rotate(90deg) translateY(-110px);
+    font-family: ${font.light};
+    letter-spacing: 1px;
+
+`}
+`
+
+export const StyledLogoMobileContainer = styled.div`
+  
+  display: flex;
+  justify-content: center ;
+  align-items: center;
+  width: 100vw;
+  height: 42%;
+  position: absolute;
+  top: 0;
+
+.container-logo{
+  width: 25vw;
+}
+${above.medium`
+
+.container-logo{
+  width: 18vw;
+}
+
+`}
+${above.large`
+
+  display: none;
 
   `}
 `
-
 
 export const StyledTitleContainer = styled.div`
-  
+  width: 100%;
+  background: none;
+  font-size: 1rem;
+  height: auto;
+  align-items: center;
+  display: list-item flow ;
+  justify-content: center ;
+  margin-top: 1.5rem;
+
+  color: ${colors.white};
+
+  h1{
+    font-family: ${font.bold};
+    font-size: 4.5rem ;
+  }
+  h3{
+    font-family: ${font.light};
+  }
+
   ${above.medium`
+  
+  h1{
+    font-size: 3.5rem ;
+  }
       
   `}
   ${above.large`
-    width: 100%;
-    background: none;
-    font-size: 1rem;
-    height: fit-content;
-    align-items: center;
-    
-    display: list-item flow ;
-    justify-content: center ;
-    font-family: 'Montserrat', sans-serif;
+
+  .container-logo{
+    display: none;
+  }
+  .logo{
+    display: none;
+  }
 
   `}
 `
-
 
 export const StyledButtonsContainer = styled.div`
   
+  width: 100%;
+  display: inline block ;
+  align-items: center;
+  height: fit-content ;
+  justify-content: center ;
+  font-family: ${font.bold}
+  margin-top: 5rem; 
+  
   ${above.medium`
-      
+
   `}
   ${above.large`
-    width: 100%;
+  
     display: flex ;
-    background: none;
-    align-items: center;
-    height: fit-content ;
-    justify-content: center ;
-    font-family: 'Montserrat', sans-serif;
-
   `}
 `
 
